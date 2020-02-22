040ff17274033d442422550f69ccda8964ea9989
Ricardo Castaneda <rikaxd96@gmail.com>
2020-02-21 23:12:48 -0800

    fetch on client side

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,19 @@
-console.log("Client side javascript file is loaded");
+// API to fetch from : puzzle.mead.io/puzzle
+
+// fetch("http://puzzle.mead.io/puzzle").then(response => {
+//   return response.json().then(data => {
+//     console.log(data);
+//   });
+// });
+
+const url = "http://localhost:3000/weather/?address=boston";
+fetch(url).then(response => {
+  return response.json().then(data => {
+    if (data.error) {
+      console.log(data.error);
+    } else {
+      console.log(data.location);
+      console.log(data.forecast);
+    }
+  });
+});
